5baee4c972beb0497c410e844e7d9f95b6c0dc28
Venelin Efremov <veffremov.ve@gmail.com>
2015-09-21 15:57:48 -0700

    Update the firmware update tool to include the bootloader files.

--- src/mkupdate.cpp
+++ src/mkupdate.cpp
@@ -10,6 +10,7 @@
 
 #include "FirmwareContainer.h"
 
+#include <dirent.h>
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <lzo/lzoconf.h>
@@ -17,25 +18,48 @@
 #include <stdint.h>
 
 DEFINE_string(update_version, "", "Version string for this update package.");
-DEFINE_string(file_prefix, "OpenBT-RPi.arm-6.0-dev", "Prefix for the file names.");
-DEFINE_string(output_path, "iqfw_update.fwu", "Output filename.");
+DEFINE_string(output_path, "iqjs.fwu", "Output filename.");
 
+using gflags::ParseCommandLineFlags;
+using gflags::SetUsageMessage;
+using google::InitGoogleLogging;
+using iqurius::FirmwareContainerWriter;
+using std::string;
+
+void addFolder(const string& path,
+		const string& dest_dir,
+		FirmwareContainerWriter* writer) {
+  DIR* dir = opendir(path.c_str());
+
+  if (!dir) {
+	  LOG(ERROR) << "Can not open " << path << " errno=" << errno;
+	  return;
+  }
+  for (struct dirent *d = readdir(dir); d; d = readdir(dir)) {
+	  if (d->d_type != DT_REG) continue;
+	  string src_path(path);
+	  src_path.append("/");
+	  src_path.append(d->d_name);
+
+	  string dest_path(dest_dir);
+	  dest_path.append(d->d_name);
+      LOG(INFO) << "Adding " << src_path << " as " << dest_path;
+      writer->addFile(src_path.c_str(), dest_path.c_str(), false);
+  }
+}
 int main(int argc, char *argv[]) {
-	gflags::SetUsageMessage("Create a firmware update container file");
-	gflags::ParseCommandLineFlags(&argc, &argv, true);
-	google::InitGoogleLogging(argv[0]);
+	SetUsageMessage("Create a firmware update container file");
+	ParseCommandLineFlags(&argc, &argv, true);
+	InitGoogleLogging(argv[0]);
 	if (lzo_init() != LZO_E_OK) {
 		LOG(ERROR) << "Error initializing the LZO library";
 		return 1;
 	}
-	iqurius::FirmwareContainerWriter writer(FLAGS_update_version.c_str());
-	std::string file_name;
-	file_name = FLAGS_file_prefix;
-	file_name.append(".kernel");
-	writer.addFile(file_name.c_str(), "kernel.img", false);
-	file_name = FLAGS_file_prefix;
-	file_name.append(".system");
-	writer.addFile(file_name.c_str(), "SYSTEM", false);
+	FirmwareContainerWriter writer(FLAGS_update_version.c_str());
+	writer.addFile("target/KERNEL", "kernel.img", false);
+	writer.addFile("target/SYSTEM", "SYSTEM", false);
+	addFolder("3rdparty/bootloader", "", &writer);
+	addFolder("3rdparty/bootloader/overlays", "overlays/", &writer);
 	writer.writeContainer(FLAGS_output_path.c_str());
 	return 0;
 }
